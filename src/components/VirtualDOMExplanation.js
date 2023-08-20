c121b283250586622aa2941cab27b1ee51c95303
Shehan Maxwell <shehanbbn@gmail.com>
2023-08-20 19:40:35 +0530

    added VirtualDOMExplanation.js to components

---
+++ src/components/VirtualDOMExplanation.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class VirtualDOMEXplanation extends React.Component {
+    constructor() {
+        super();
+        // Initialize state
+
+        this.state = {
+            count: 0,
+        }
+    }
+}
