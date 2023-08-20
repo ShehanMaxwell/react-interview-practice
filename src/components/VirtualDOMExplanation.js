fb0c46e0a3d3541746623a9c366f358932ec957c
Shehan Maxwell <shehanbbn@gmail.com>
2023-08-20 22:25:09 +0530

    modified App.js and VirtualDOMExplanation.js

--- src/components/VirtualDOMExplanation.js
+++ src/components/VirtualDOMExplanation.js
@@ -1,27 +1,28 @@
 import React from 'react';
 
 class VirtualDOMEXplanation extends React.Component {
-    constructor() {
-        super();
-        // Initialize state
+  constructor() {
+    super();
+    // Initialize state
 
-        this.state = {
-            count: 0,
-        }
-    }
-    // Event handler to update the state
-    handleIncrement = () => {
-        this.setState({count: this.state.count + 1})
-    }
+    this.state = {
+      count: 0,
+    };
+  }
+  // Event handler to update the state
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
 
-    render() {
-        return (
-            <div>
-                <h1>Virtual DOM Example</h1>
-                <p>Count: {this.state.count}</p>
-                <button></button>
-            </div>
-        )
-    }
-
+  render() {
+    return (
+      <div>
+        <h1>Virtual DOM Example</h1>
+        <p>Count: {this.state.count}</p>
+        <button onClick={this.handleIncrement}>Increment</button>
+      </div>
+    );
+  }
 }
+
+export default VirtualDOMEXplanation;
